46ebee11ccc1f1a02281bc673bb7072730570f01
AValeriaCM <anvalecasmo@outlook.es>
2021-02-23 18:54:09 -0500

    Ajuste Cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,7 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-        ->header("Access-Control-Allow-Origin", "http://visas.ddns.net")
+        ->header("Access-Control-Allow-Origin", "http://localhost:4200")
         ->header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
         ->header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, X-Token-Auth, Authorization");
     }
