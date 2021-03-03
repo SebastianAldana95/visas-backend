6417a90acda3f8db376817010cd41284a631909d
AValeriaCM <anvalecasmo@outlook.es>
2021-03-03 16:16:58 -0500

    ajustes comprobante

--- resources/views/livewire/ventas/create.blade.php
+++ resources/views/livewire/ventas/create.blade.php
@@ -19,32 +19,43 @@
                         </div>
                         <div class="mb-4">
                             <label for="exampleFormControlInput2" class="block text-gray-700 text-sm font-bold mb-2">Nombre:</label>
-                            <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="exampleFormControlInput2" wire:model="name" placeholder="Nombre"></textarea>
+                            <input type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="exampleFormControlInput1" placeholder="Nombre" wire:model="name" required>
                             @error('name') <span class="text-red-500">{{ $message }}</span>@enderror
                         </div>
                         <div class="mb-4">
                             <label for="exampleFormControlInput2" class="block text-gray-700 text-sm font-bold mb-2">Cedula:</label>
-                            <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="exampleFormControlInput2" wire:model="identification" placeholder="Cedula"></textarea>
+                            <input type="number" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="exampleFormControlInput1" placeholder="Documento Identificacion" wire:model="identification" required>
                             @error('identification') <span class="text-red-500">{{ $message }}</span>@enderror
                         </div>
                         <div class="mb-4">
                             <label for="exampleFormControlInput2" class="block text-gray-700 text-sm font-bold mb-2">Correo:</label>
-                            <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="exampleFormControlInput2" wire:model="email" placeholder="Correo Electronico"></textarea>
+                            <input type="email" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="exampleFormControlInput1" placeholder="Correo electrónico" wire:model="email" required>
                             @error('email') <span class="text-red-500">{{ $message }}</span>@enderror
                         </div>
                         <div class="mb-4">
                             <label for="exampleFormControlInput2" class="block text-gray-700 text-sm font-bold mb-2">Cantidad:</label>
-                            <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="exampleFormControlInput2" wire:model="amount" placeholder="Cantidad"></textarea>
+                            <input type="number" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="exampleFormControlInput1" placeholder="Cantidad" wire:model="amount" required>
                             @error('amount') <span class="text-red-500">{{ $message }}</span>@enderror
                         </div>
                         <div class="mb-4">
-                            <label for="exampleFormControlInput2" class="block text-gray-700 text-sm font-bold mb-2">Servicio:</label>
-                            <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="exampleFormControlInput2" wire:model="service" placeholder="Servicio"></textarea>
+                                <!-- This example requires Tailwind CSS v2.0+ -->
+                                <div class="relative inline-flex">
+                                <label for="exampleFormControlInput2" class="block text-gray-700 text-sm font-bold mb-2">Servicio:&nbsp;&nbsp;&nbsp;</label>
+                                    <svg class="w-2 h-2 absolute top-0 right-0 m-4 pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232"><path d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z" fill="#648299" fill-rule="nonzero"/></svg>
+                                    <select class="border border-gray-300 rounded-full text-gray-600 h-10 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none" wire:model="service">
+                                        <option>ASESORIA VISA AMERICANA</option>
+                                        <option>ASESORIA VISA CANADA</option>
+                                        <option>DERECHO CONSULAR VISA AMERICANA</option>
+                                        <option>DERECHO CONSULAR VISA CANADIENSE</option>
+                                        <option>CAMBIO DE FORMULARIOS </option>
+                                    </select>
+                                </div>
+
                             @error('service') <span class="text-red-500">{{ $message }}</span>@enderror
                         </div>
-                        <div class="mb-4">
+                        <div class="mb-4"> 
                             <label for="exampleFormControlInput2" class="block text-gray-700 text-sm font-bold mb-2">Zona:</label>
-                            <textarea class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="exampleFormControlInput2" wire:model="zone" placeholder="Zona"></textarea>
+                            <input type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="exampleFormControlInput1" placeholder="EEUU / Canadá" wire:model="zone" required>
                             @error('zone') <span class="text-red-500">{{ $message }}</span>@enderror
                         </div>
                         <!--Colocar los demas campos del formulario-->
--- resources/views/livewire/ventas/invoice.blade.php
+++ resources/views/livewire/ventas/invoice.blade.php
@@ -9,44 +9,71 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="public/css/stylepdf.css">
 
-    <title>Ventas</title>
+    <title>COMPROBANTE DE PAGO V- 0289_2021</title>
 </head>
 <body>
     <div>
-        <header class="header">
-        <img src="https://visascontinental.com/wp-content/uploads/2020/08/LOGOS-VISAS-CONTINENTAL-02-e1597271914384.png" style="height: 80px;"/>
-            <p style="text-align: center"><strong>Comprobante de Pago</strong></p>
-            <p style="text-align: right">www.visascontinental.com</p>
-            <p style="text-align: right">Cra 11B # 99 - 25 Bogotá, Colombia</p>
-        </header>
-        <table class="table-auto" style="padding-left: 4px">
+            <div class="flex flex-row">
+                <div>
+                    <img src="https://visascontinental.com/wp-content/uploads/2020/08/LOGOS-VISAS-CONTINENTAL-02-e1597271914384.png" style="height: 80px; width: 250px;"/>
+                </p>  
+                </div>
+                <div >
+                    
+                </div>
+                              
+                <div >
+                    <p>www.visascontinental.com
+                    <br>Telefono: 302 2155592 
+                    <br>Cra 11B # 99 - 25 Bogotá, Colombia
+                    <br>Email: asesorias@visascontinental.com</p>
+                </div>
+
+            </div>
+
+        <h4 style="text-align: center; color: darkblue; font-weight: bold;">Comprobante de pago</h4>
+
+        <table class="table-auto w-full" style="margin: 30px;">
         @foreach($sales as $sale)
             <thead>
             <tr>
-                <th scope="col">Cliente</th>
-                <th scope="row">{{ $sale->name }}</th>
+                <th class="px-4 py-2">Cliente:</th>
+                <th class="px-4 py-2"></th>
+                <th class="px-4 py-2"></th>
+                <th class="px-4 py-2"></th>
+                <th class="px-4 py-2">{{ $sale->name }}</th>
             </tr>
             </thead>
             <tbody>
                     <tr>
                         <td>C.C </td>
+                        <td></td>
+                        <td></td><td></td>
                         <td>{{ $sale->identification }}</td>
                         
                     </tr>
                     <tr>
                         <td>Correo: </td>
+                        <td></td>
+                        <td></td><td></td>
                         <td>{{ $sale->email }}</td>
                     </tr>
                     <tr>
                         <td>Vendedor: </td>
+                        <td></td>
+                        <td></td><td></td>
                         <td>{{ $sale->amount }}</td>
                     </tr>
                     <tr>
                         <td>Servicio:</td>
+                        <td></td>
+                        <td></td><td></td>
                         <td>{{ $sale->service }}</td>
                     </tr>
                     <tr>
                         <td>Zona:</td>
+                        <td></td>
+                        <td></td><td></td>
                         <td>{{ $sale->zone }}</td>
                     </tr>
             </tbody>
